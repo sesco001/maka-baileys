79d73a58f5aaa2dc07224b828f770f1840c2f2f1
Maurice Gift <founder@giftedtech.co.ke>
2026-01-31 12:38:39 +0300

    Initial commit

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const baileys = require('./lib/index.js');
 
-console.log('=== keith-baileys Library Test ===\n');
+console.log('=== gifted-baileys Library Test ===\n');
 console.log('Library loaded successfully!');
 console.log('\nAvailable exports:');
 console.log(Object.keys(baileys).join('\n'));
